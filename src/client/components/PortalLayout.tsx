1a097ec913f3f24d19876596cff84321c4ad187b
Lutrick <m.lutrick@mitsui.com>
2026-05-28 12:18:51 -0500

    Lets get some toast

--- src/client/components/PortalLayout.tsx
+++ src/client/components/PortalLayout.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { type ReactNode } from 'react'
 import PortalSidebar from './PortalSidebar'
 import { PORTAL_HOME_PATH } from '../utils/portalPage'
 
 type PortalLayoutProps = {
     children: React.ReactNode
+    toast?: ReactNode
 }
 
-export default function PortalLayout({ children }: PortalLayoutProps) {
+export default function PortalLayout({ children, toast }: PortalLayoutProps) {
     return (
         <div className="portal-shell font-sans">
             <header className="portal-header">
                 <a href={PORTAL_HOME_PATH} className="portal-header-title">
                     Ticket Intake Portal
                 </a>
+                {toast && <div className="portal-toast-region">{toast}</div>}
             </header>
             <div className="portal-body">
                 <PortalSidebar />
